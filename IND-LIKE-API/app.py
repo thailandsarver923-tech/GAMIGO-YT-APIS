883fd2615b3bc0513583cb5f002faad272a87416
thailandsarver923-tech <thailandsarver923@gmail.com>
2025-10-29 18:28:30 +0530

    Update app.py

--- IND-LIKE-API/app.py
+++ IND-LIKE-API/app.py
@@ -195,7 +195,7 @@ def make_request(encrypt, server_name, token):
             'Expect': "100-continue",
             'X-Unity-Version': "2018.4.11f1",
             'X-GA': "v1 1",
-            'ReleaseVersion': "OB50"
+            'ReleaseVersion': "OB51"
         }
         response = requests.post(url, data=edata, headers=headers, verify=False)
         hex_data = response.content.hex()
