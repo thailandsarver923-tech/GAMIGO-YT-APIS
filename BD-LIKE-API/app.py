f3b0c80a527c8b3b32e2433ef8ca3fa1d1aceb2d
thailandsarver923-tech <thailandsarver923@gmail.com>
2025-10-30 13:02:26 +0530

    Update app.py

--- BD-LIKE-API/app.py
+++ BD-LIKE-API/app.py
@@ -123,7 +123,7 @@ async def send_request(encrypted_uid, token, url):
             'Expect': "100-continue",
             'X-Unity-Version': "2018.4.11f1",
             'X-GA': "v1 1",
-            'ReleaseVersion': "OB51"
+            'ReleaseVersion': "OB50"
         }
         async with aiohttp.ClientSession() as session:
             async with session.post(url, data=edata, headers=headers) as response:
